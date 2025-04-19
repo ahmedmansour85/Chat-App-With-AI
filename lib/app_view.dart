75c37c6696918c56568ecebe1ae7f7af8a934f0f
ahmedmansour85 <ahmedarbeed85@gmail.com>
2025-04-19 21:48:37 +0300

    add some changes ❤

---
+++ lib/app_view.dart
@@ -0,0 +1,15 @@
+import 'package:chat_app_with_ai/feature/home/ui/homw_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {    
+    return const MaterialApp(
+      home: HomwScreen(),
+    );
+  }
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:chat_app_with_ai/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
